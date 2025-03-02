d07980fc0546b5963c0673e239c201784b5428d1
Bo Long <longt2202@gmail.com>
2025-03-02 16:27:14 +0700

    Add paths to all subfiles inside

---
+++ features/__init__.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+# Add path to helpers/
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
+
+# Add sys.path to import environment.py
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+sys.path.append(parent_dir)
